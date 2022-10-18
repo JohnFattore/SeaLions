f0fe0833c824c6b4c0d5f7c106cd103c3fc85467
torkfat <105743305+torkfat@users.noreply.github.com>
2022-10-18 13:52:12 -0400

    Added encounter addition, No SQL

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # SeaLions
 SeaLions Flask Application
-
-# Turorial
-Here is the tutorial I am following: https://pythonhow.com/python-tutorial/flask -Mei
--- app/app.py
+++ app/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('home.html')
-@app.route('/about/')
+    return render_template('index.html')
+@app.route('/about/', methods=["GET","POST"])
 def about():
-    return render_template('about.html')
+    if request.method == "POST":
+        if not request.form.get("name"):
+            return render_template("apology.html")
+
+        if not request.form.get("time"):
+            return render_template("apology.html")
+            
+        name = request.form.get("name")
+        time = request.form.get("time")        
+        return render_template("aboutpost.html", name=name, time=time)
+    else:
+        return render_template("about.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
